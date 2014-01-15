a3b593518d0568bf0836fe16decb17296c7dab8d
qiyiping <qiyiping@gmail.com>
2014-01-15 16:27:22 +0800

    import auc from mahout

---
+++ src/cpp/auc.hpp
@@ -0,0 +1,92 @@
+// Author: qiyiping@gmail.com (Yiping Qi)
+
+#ifndef _AUC_H_
+#define _AUC_H_
+#include "data.hpp"
+#include <vector>
+
+namespace gbdt {
+// imported from mahout:
+// org.apache.mahout.classifier.evaluation.Auc
+
+class Auc {
+ public:
+  Auc() {}
+
+  void Add(ValueType score, ValueType label) {
+    if (label > 0)
+      positive_scores.push_back(score);
+    else
+      negative_scores.push_back(score);
+  }
+
+  ValueType CalculateAuc() const {
+    std::sort(negative_scores);
+    std::sort(positive_scores);
+
+    ValueType n0 = negative_scores.size();
+    ValueType n1 = positive_scores.size();
+
+    if (n0 == 0 || n1 == 0) {
+      return 0.5;
+    }
+
+    // scan the data
+    int i0 = 0;
+    int i1 = 0;
+    int rank = 1;
+    ValueType rankSum = 0;
+    while (i0 < n0 && i1 < n1) {
+
+      ValueType v0 = negative_scores.[i0];
+      ValueType v1 = positive_scores.[i1];
+
+      if (v0 < v1) {
+        i0++;
+        rank++;
+      } else if (v1 < v0) {
+        i1++;
+        rankSum += rank;
+        rank++;
+      } else {
+        // ties have to be handled delicately
+        ValueType tieScore = v0;
+
+        // how many negatives are tied?
+        int k0 = 0;
+        while (i0 < n0 && negative_scores[i0] == tieScore) {
+          k0++;
+          i0++;
+        }
+
+        // and how many positives
+        int k1 = 0;
+        while (i1 < n1 && positive_scores[i1] == tieScore) {
+          k1++;
+          i1++;
+        }
+
+        // we found k0 + k1 tied values which have
+        // ranks in the half open interval [rank, rank + k0 + k1)
+        // the average rank is assigned to all
+        rankSum += (rank + (k0 + k1 - 1) / 2.0) * k1;
+        rank += k0 + k1;
+      }
+    }
+
+    if (i1 < n1) {
+      rankSum += (rank + (n1 - i1 - 1) / 2.0) * (n1 - i1);
+      rank += (int) (n1 - i1);
+    }
+
+    return (rankSum / n1 - (n1 + 1) / 2) / n0;
+  }
+
+ private:
+  std::vector<ValueType> positive_scores;
+  std::vector<ValueType> negative_scores;
+};
+
+}
+
+#endif /* _AUC_H_ */
