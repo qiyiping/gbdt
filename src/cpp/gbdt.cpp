3cda651626b9a0d5fea2fbfedd37839cf0637650
qiyiping <qiyiping@gmail.com>
2014-01-15 14:50:54 +0800

    add gbdt unittest

--- src/cpp/gbdt.cpp
+++ src/cpp/gbdt.cpp
@@ -1,4 +1,8 @@
 // Author: qiyiping@gmail.com (Yiping Qi)
+#include "gbdt.hpp"
+#include "fitness.hpp"
+#include <iostream>
+#include <cmath>
 
 namespace gbdt {
 ValueType GBDT::Predict(const Tuple &t) const {
@@ -7,7 +11,7 @@ ValueType GBDT::Predict(const Tuple &t) const {
 
   ValueType r = 0;
   for (int i = 0; i < gConf.iterations; ++i) {
-    r += gConf.shrinkage * tree[i].Predict(t);
+    r += gConf.shrinkage * trees[i].Predict(t);
   }
 
   return r;
@@ -16,9 +20,18 @@ ValueType GBDT::Predict(const Tuple &t) const {
 void GBDT::Fit(DataVector *d) {
   trees = new RegressionTree[gConf.iterations];
   for (int i = 0; i < gConf.iterations; ++i) {
-    tree[i].Fit(d);
+    trees[i].Fit(d);
 
-    
+    DataVector::iterator iter = d->begin();
+    ValueType s = 0;
+    for ( ; iter != d->end(); ++iter) {
+      ValueType p = trees[i].Predict(**iter);
+      (*iter)->target -= gConf.shrinkage * p;
+      s += Squared((*iter)->target);
+    }
+
+    std::cout  << "iteration: " << i << std::endl
+               << "rmse: " << std::sqrt(s/d->size()) << std::endl;
   }
 }
 }
---
+++ src/cpp/gbdt_unittest.cpp
@@ -0,0 +1,48 @@
+#include "gbdt.hpp"
+#include "fitness.hpp"
+#include <iostream>
+#include <cassert>
+#include <boost/lexical_cast.hpp>
+
+using namespace gbdt;
+
+int main(int argc, char *argv[]) {
+  gConf.number_of_feature = 3;
+  gConf.max_depth = 4;
+  gConf.iterations = 10;
+  gConf.shrinkage = 0.1;
+
+  if (argc > 1) {
+    gConf.max_depth = boost::lexical_cast<int>(argv[1]);
+  }
+
+  if (argc > 2) {
+    gConf.iterations = boost::lexical_cast<int>(argv[2]);
+  }
+
+  if (argc > 3) {
+    gConf.shrinkage = boost::lexical_cast<float>(argv[3]);
+  }
+
+  DataVector d;
+  bool r = LoadDataFromFile("../../data/test.dat", &d);
+  assert(r);
+
+  GBDT gbdt;
+
+  gbdt.Fit(&d);
+
+  DataVector::iterator iter = d.begin();
+  PredictVector predict;
+  for ( ; iter != d.end(); ++iter) {
+    ValueType p = gbdt.Predict(**iter);
+    predict.push_back(p);
+    std::cout << (*iter)->ToString() << std::endl
+              << p << std::endl;
+  }
+
+  std::cout << "rmse: " << RMSE(d, predict) << std::endl;
+
+  CleanDataVector(&d);
+  return 0;
+}
