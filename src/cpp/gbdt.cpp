7e382ad7dd71203d2dfbe4d9502eb64601b83787
qiyiping <qiyiping@gmail.com>
2014-01-21 22:13:15 +0800

    update gbdt.cpp

--- src/cpp/gbdt.cpp
+++ src/cpp/gbdt.cpp
@@ -104,11 +104,6 @@ void GBDT::Fit(DataVector *d) {
       }
     }
 
-    // tune split process
-    if (i > 3) {
-      g_conf.enable_feature_tunning = false;
-    }
-
     trees[i].Fit(d, samples);
   }
 }
